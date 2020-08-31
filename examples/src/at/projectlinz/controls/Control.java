58a40f3ee1b7c5da10749c8bfd313d4e47e175a0
Bahman-66 <bahman.bahman_zangi@jku.at>
2020-09-01 01:33:02 +0200

    closes #17

--- examples/src/at/projectlinz/controls/Control.java
+++ examples/src/at/projectlinz/controls/Control.java
@@ -1,25 +1,30 @@
 package at.projectlinz.controls;
 
-import java.util.concurrent.ExecutorService;
+import java.util.Map;
 
 import at.projectlinz.hardware.IMotor;
+import at.projectlinz.listeners.events.Event;
+import at.projectlinz.motorhandler.MotorHandler;
 
 public class Control {
 
-	private ExecutorService e;
-	private IMotor[] m;
+	private MotorHandler handler;
 
-	public Control(IMotor[] motors, ExecutorService service) {
-		this.m = motors;
-		this.e = service;
+	public Control(MotorHandler motorHandler) {
+		this.handler = motorHandler;
 	}
 
-	public ExecutorService getExecuter() {
-		return e;
+	public Map<String, IMotor> getMotors() {
+		return this.handler.getRobot().getMotors();
 	}
 
-	public IMotor[] getMotors() {
-		return m;
+	public void sendEvent(Event event) {
+		try {
+			handler.updateMotor(event);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
---
+++ examples/src/at/projectlinz/listeners/events/MoveDirection.java
@@ -0,0 +1,5 @@
+package at.projectlinz.listeners.events;
+
+public enum MoveDirection {
+	RIGHT, LEFT, FORWARD;
+}
